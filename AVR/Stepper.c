90f597e012d208df160ecf486f322d5dc0f24abc
SloopySky <61380776+SloopySky@users.noreply.github.com>
2021-02-02 14:53:16 +0100

    Add files via upload

--- AVR/Stepper.c
+++ AVR/Stepper.c
@@ -3,12 +3,11 @@
 #include "Time.h"
 #include "Stepper.h"
 
-#define abs(x) ((x)<0 ? -(x):(x))
 #define PORT(x) (*x)		// Pin state register (HIGH or LOW)
 #define DDR(x) (*(x - 1))	// Data Direction Register (input or output)
 
 // Max number of steppers
-#define MAX_STEPPERS			10
+#define MAX_STEPPERS			5
 
 // Default value of field max_speed in deg/s
 #define MAX_SPEED_DEFAULT 		10
@@ -17,102 +16,133 @@
 #define CW		 		 1
 #define CCW				-1
 
+/*
+*/
+static bool step(void);
+
+/*
+*/
+static void set_direction(uint8_t index, int8_t dir);
+
 struct Stepper {
 	volatile uint8_t *port;
 	uint8_t step_pin;
 	uint8_t dir_pin;
+	bool reverse_direction;		// Set True to reverse direction
+	uint32_t steps;			// [steps]
 	float deg_per_step;		// [deg/step]
-	int32_t current_position;	// [steps]
-	int32_t target_position;	// [steps]
-	int8_t direction;		// CW or CCW
-	float max_speed;		// [deg/s]
+	float max_speed;		// [steps/us]
 	float interval;			// [us]
 	uint32_t last_step_time;	// [us]
 } steppers[MAX_STEPPERS];
 
 static uint8_t g_stepper_count = 0;
 
-static bool step(void) {
-	bool ret = false;
-	for (uint8_t i = 0; i < g_stepper_count; i++) {
-		if (steppers[i].current_position != steppers[i].target_position) {
-			uint32_t time = micros();   
-    			if (time - steppers[i].last_step_time >= steppers[i].interval) {
-				if (PORT(steppers[i].port) & (1 << steppers[i].step_pin)) {
-					PORT(steppers[i].port) &= ~(1 << steppers[i].step_pin);
-					steppers[i].current_position += steppers[i].direction;
-				}
-				else PORT(steppers[i].port) |= (1 << steppers[i].step_pin);
-				steppers[i].last_step_time = time;
-			}
-			ret = true;
-		}
-	}
-	return ret;
-}
-
-uint8_t add_stepper(volatile uint8_t *port, uint8_t step_pin, uint8_t dir_pin, uint8_t stepping, float deg_per_step) {
+uint8_t add_stepper(volatile uint8_t *port, uint8_t step_pin, uint8_t dir_pin, float deg_per_step) {
 	if (!g_stepper_count) init_time();
 	
-	steppers[g_stepper_count].port = port;
+	if (deg_per_step <= 0.0) deg_per_step = 1.0;		// deg_per_step is a physical factor, 
+								// must be greater than 0 
+	steppers[g_stepper_count].port = port;		
 	steppers[g_stepper_count].step_pin = step_pin;
 	steppers[g_stepper_count].dir_pin = dir_pin;
-	steppers[g_stepper_count].deg_per_step = deg_per_step / stepping;
-	steppers[g_stepper_count].current_position = 0;
-	steppers[g_stepper_count].target_position = 0;
-	steppers[g_stepper_count].max_speed = MAX_SPEED_DEFAULT;
+	steppers[g_stepper_count].deg_per_step = deg_per_step;
+	steppers[g_stepper_count].reverse_direction = 0;	// Direction not reversed as default
+	
+	// Set default speed 
+	set_speed(g_stepper_count, MAX_SPEED_DEFAULT);
 
-	//Enable outputs
+	// Enable outputs
 	DDR(steppers[g_stepper_count].port) |= (1 << steppers[g_stepper_count].step_pin) \
 					    |  (1 << steppers[g_stepper_count].dir_pin);
-	
-	return(++g_stepper_count);
+
+	return(++g_stepper_count);	// Increment and return number of motors initialized so far	
 }
 
-uint32_t move_angles(const float angles[]) {
-	static int32_t steps[g_stepper_count];
-	for (i = 0; i < g_stepper_count; i++)
-		steps[i] = angles[i] / steppers[i].deg_per_step;
-	
-	return move_steps(steps);
-}
-
-uint32_t move_steps(const int32_t steps[]) {
+uint32_t motion(const int32_t angles[]) {
 	float longest_time = 0.0;
 	uint8_t i;
 	for (i = 0; i < g_stepper_count; i++) {
+		if (angles[i] >= 0) {
+			set_direction(i, CW);
+			steppers[i].steps = angles[i] / steppers[i].deg_per_step;	
+		} 
+		else {
+			set_direction(i, CCW);
+			steppers[i].steps = -angles[i] / steppers[i].deg_per_step;
+		}
+
 		float this_time = 0.0;
 		if (steppers[i].max_speed)
-			this_time = abs(steps[i]) * 1000000.0 * steppers[i].deg_per_step / steppers[i].max_speed;
+			this_time = steppers[i].steps / steppers[i].max_speed;
 		if (this_time > longest_time) longest_time = this_time;
-	
-		if (steps[i] >= 0) {
-			PORT(steppers[i].port) &= ~(1 << steppers[i].dir_pin);
-			steppers[i].direction = CW;
-		}
-		else {
-			PORT(steppers[i].port) |= (1 << steppers[i].dir_pin);
-			steppers[i].direction = CCW;
-		}
 	}
-
 	if (longest_time > 0.0) {
-		// Now work out a new max speed for each stepper so they will all 
+		// Work out a new interval for each stepper so they will all 
 		// arrived at the same time of longest_time
 		for (i = 0; i < g_stepper_count; i++) {
 			if (steps[i] == 0) steppers[i].interval = longest_time;
-			else steppers[i].interval = longest_time / (2.0 * abs(steps[i]));
-			steppers[i].target_position = steppers[i].current_position + steps[i];
+			else steppers[i].interval = longest_time / (2.0 * steppers[i].steps);
 		}
 	}
 	return longest_time;
 }
-   
-void run(void) {
-	while(step());
+
+static bool step(void) {
+	bool ret = false;	// Assume that all motors have finished
+	for (uint8_t i = 0; i < g_stepper_count; i++) {
+		if (steppers[i].steps) {
+			// There is at least one step to be performed
+			// Read current time in microseconds	
+			uint32_t time = micros();	
+    			if (time - steppers[i].last_step_time >= steppers[i].interval) {
+				// It's time to step
+				// Generate square wave
+				// If step pin state is high set it low
+				if (PORT(steppers[i].port) & (1 << steppers[i].step_pin)) {
+					PORT(steppers[i].port) &= ~(1 << steppers[i].step_pin);
+					steppers[i].steps--;	// Count steps on trailing edge
+				}
+				// If step pin state is low set it high
+				else PORT(steppers[i].port) |= (1 << steppers[i].step_pin);
+				// Remember time of this step
+				steppers[i].last_step_time = time;	 
+			}
+			ret = true;	// At least one motor hasn't finished yet
+		}
+	}
+	// Return true if at least one motor hasn't finished yet, otherwise false
+	return ret;
 }
 
-void set_speed(uint8_t i, float speed) {
-	if (speed < 0) steppers[i].max_speed = 0.0;
-	else steppers[i].max_speed = speed;
-}	
+void run(void) {
+	while(step());	// Run motors until all of them arrive
+}
+
+static void set_direction(uint8_t index, int8_t dir) {
+	if (g_stepper_count < index) return;
+	if (steppers[index].reverse_direction) dir = -dir;
+
+	if (dir == CW)
+		PORT(steppers[index].port) |= (1 << steppers[index].dir_pin);
+	if (dir == CCW)
+		PORT(steppers[index].port) &= ~(1 << steppers[index].dir_pin);
+}
+
+void reverse_direction(uint8_t index, bool reverse) {
+	if (g_stepper_count < index) return;		// Such stepper not initialized, exit function
+	steppers[index].reverse_direction = reverse;	// Reverse directions of specified motor
+}
+
+void set_stepping(uint8_t index, uint8_t stepping) {
+	if (g_stepper_count < index) return;			// Such stepper not initialized, exit function
+	if (stepping < 1) stepping = 1;				// Minimum stepping value is 1
+	steppers[index].deg_per_step = deg_per_step / stepping;	// Recalculates degrees per one step
+}
+
+void set_speed(uint8_t index, float speed) { 	// speed in deg/s
+	if (g_stepper_count < index) return;	// Such stepper not initialized, exit function
+	if (speed < 0) steppers[index].max_speed = 0.0;	// Speed can't be nagative
+	// Convert deg/s to steps/us and store value:
+	else steppers[index].max_speed = speed / (1000000.0 * steppers[index].deg_per_step);
+}
--- AVR/Stepper.h
+++ AVR/Stepper.h
@@ -15,36 +15,21 @@ REQUIRES:
 /*
 add_stepper() function initializes a stepper in the array of steppers,
 writes addresses of hardware connections that are passed by arguments,
-assigns positions as zero, max_speed as default.
-deg_per_step argument is a stepper motor parameter.
-stepping argument value depends of physical microstepping resolution as follows: 
-1: full step
-2: half step
-4: quarter step, etc.
+sets default speed.
+deg_per_step argument is a stepper motor parameter,
+that defines degrees of rotation that is caused by a single step signal.
 At the first call initializes timer that is used to drive steppers.
 Returns number of steppers that are initialized so far.
 */
-uint8_t add_stepper(volatile uint8_t *port, uint8_t step_pin, uint8_t dir_pin, \
-					uint8_t stepping, float deg_per_step);
+uint8_t add_stepper(volatile uint8_t *port, uint8_t step_pin, uint8_t dir_pin, float deg_per_step);
 
 /*
-move_steps() function doesn't perform any motion,
-just prepares parameters for the future motion.
-Calculates and returns duration time of the motion.
-For each stepper:
-* sets direction field and PIN to the right values
-* calculates interval time between steps and the target position of the movement.
-Argument steps[] should contain array of steps to be performed
-by corresponding stepper.
-*/
-uint32_t move_steps(const int32_t steps[]);
-
-/*
-move_angles() function calculates amount of steps to perform in order to obtain
-angular displacement passed by argument array angles[], then calls move_steps().
+motion() function doesn't perform any motion,
+just prepares parameters for the future simultaneous motion
+of each motor by the corresponding angle from array that is spassed as argument.
 Returns duration time of the motion.
 */
-uint32_t move_angles(const float angles[])
+uint32_t motion(const float angles[]);
 
 /*
 run() function performs steps
@@ -53,8 +38,24 @@ unless all steppers reach their target positions.
 void run(void);
 
 /*
-set_speed() function assigns the max_speed field
-of i-index Stepper struct from steppers array
+reverse_direction() with argument value of True reverses direction of rotation
+of the motor specified by index argument, so that CW becomes CCW and vice versa.
+False value of argument restores directions. 
+*/
+void reverse_direction(uint8_t index, bool reverse);
+
+/*
+set_stepping() allows to change physical microstepping resolution.
+Specific values of stepping argument correspond with the adequate mode as follows: 
+1: full step
+2: half step
+4: quarter step, etc.
+*/
+void set_stepping(uint8_t index, uint8_t stepping);
+
+/*
+set_speed() function stores the speed value (in deg/s) passed as argument
+of i-index motor
 with function argument speed in deg/s.
 */
 void set_speed(uint8_t i, float speed);
@@ -64,12 +65,12 @@ void set_speed(uint8_t i, float speed);
 #if 0
 EXAMPLE OF USE:
 
-add_stepper(&PORTB, PB6, PB7, 4, 1.0/1.8);	// stepper index = 0	
-add_stepper(&PORTB, PB1, PB2, 4, 1.0/1.8);	// stepper index = 1	
+add_stepper(&PORTB, PB6, PB7, 4, 1.8);		// stepper index = 0	
+add_stepper(&PORTB, PB1, PB2, 4, 1.8);		// stepper index = 1	
 
 float angles[] = {30, 90};			// stepper 0 angle = 30, stepper 1 angle = 90
 
-move_angles(angles);				// calculate
+motion(angles);					// calculate
 run();						// move
 
 #endif
